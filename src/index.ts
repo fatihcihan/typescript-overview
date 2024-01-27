eaf74b9d60c7f8685bd4ebb9b800a4fb109093e0
Fatih <fatihcihan666@gmail.com>
2024-01-27 19:43:35 +0300

    basic examples of typing (string, number, boolean)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-console.log('hello world');
-let x = 10, y = 20;
-let myAdd = (x: number, y: number): number => {
-    return x + y;
-};
-myAdd(x, y);
+let name: string = 'name 1';
+name = "name 2"
+// name = 30;      // type 'number' is not assignable to type 'string'
+
+
+let test: string | number;      // supports both types
+test = "test 1"
+test = 30;
+// console.log(typeof test);       // number
+
+let myBool = true;
+myBool = !myBool;
+myBool = !!name;    
+console.log(myBool);   
+// !! -> is used to convert a value to boolean type and also to check if the value is "truthy" or "falsy".
+
+
+
